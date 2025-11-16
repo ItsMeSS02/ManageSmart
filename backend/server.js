d77684b4109b4d2f480c770c5988fa114113eec5
Suharsh Sonkar <sonkarsuharsh@gmail.com>
2025-11-16 08:13:22 +0530

    remove wildcard root

--- backend/server.js
+++ backend/server.js
@@ -33,10 +33,6 @@ const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV === "production") {
   const frontendPath = path.join(__dirname, "..", "frontend", "dist");
   app.use(express.static(frontendPath));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
-  });
 }
 
 app.listen(PORT, () => console.log("Server started on port", PORT));
